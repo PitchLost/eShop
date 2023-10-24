548bbc7463ec8d494be16f5481d747a3082e31df
PitchLost <thundergames221@gmail.com>
2023-10-25 09:15:13 +1300

    Fix Button

--- Scripts/old.js
+++ Scripts/old.js
@@ -31,3 +31,76 @@ run().catch(console.dir);
 
 
 // ____ mongoDB
+
+
+
+
+
+
+
+
+
+
+
+// Post Request 
+
+
+async function sendData(username, password) {
+  console.log('Post Requested');
+  const formData = new FormData();
+  formData.append('username', username);
+  formData.append('password', password);
+  console.log('Form data value:', formData);
+
+  try {
+      const response = await fetch('http://127.0.0.1:5500/NodeServer/login', {
+          method: 'POST',
+          body: formData,
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+          }
+      });
+
+      console.log('Promise Activated');
+      
+      if (response.ok) {
+          console.log('Action seen');
+      } else {
+          console.error('ERR 999 Response failed');
+      }
+  } catch (error) {
+      console.error('The Post was rejected', error);
+  } finally {
+      console.log('The Post was neither rejected nor accepted');
+  }
+}
+
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const userNparcel = userName.value;
+  const passWparcel = passWord.value;
+  console.log(userNparcel);
+  console.log(passWparcel);
+
+  // Send the form data to the server
+  await sendData(userNparcel, passWparcel);
+
+  userName.value = null;
+  passWord.value = null; 
+});
+
+
+
+
+
+
+
+
+
+// Define the route for handling POST requests
+app.post('/NodeServer/login', (req, res) => {
+  const clientIP = req.ip;
+  console.log('The incoming req is from', clientIP);
+  res.json({ message: 'Received a POST request', data: req.body });
+});
