8c93c648d05a0be6de1d84de742a59b1559a57ec
PitchLost <thundergames221@gmail.com>
2023-11-07 11:57:36 +1300

    Update 0.1

--- Cart/Documents/plan.md
+++ Cart/Documents/plan.md
@@ -4,4 +4,9 @@ Future implications
 
 I think that by using a array we will be able to make the cart with less hardcode and just store the items in the array. 
 
-GPT gone shit on us so gonna have to figure out alone if we do decide that. 
+
+
+
+
+When a item is added it should create a new div which will then be appended to cartItems instead of cartRows. At the moment cartRows is 
+just a horizontal line instead of having any height to it. This is good for styling a single element to but not multiple. The con with increasing the height on the cartRows is it may add scroll to the page at somepoint. Which we really don't want down the bottom. So Im thinking that by creating a new div each time in appending it to cartItems and not cartRows should make it go below the cartRows div.
--- Cart/cart.js
+++ Cart/cart.js
@@ -10,26 +10,88 @@ let itemBeingAdded = undefined;
 
 // Define the callers (buttons):
 const addToCartBtns = document.querySelectorAll('.cartBtn');
-
 // Main Function to add the item
 addToCartBtns.forEach(function (button) {
     button.addEventListener('click', e => {
         // Log in the console that the function has been called
-        console.log('Add to cart Function called')
+        console.log('Add to cart Function called');
 
         // Get the parent element of the item that is being added
-
-        itemBeingAdded = button.parentElement;
-        console.log('Item being added:', itemBeingAdded);
-
+        itemBeingAdded = button.parentElement.cloneNode(true); // Clone the element
+        
         // Add the item to the back of the array
         cartArray.push(itemBeingAdded);
 
-        // Output the updated cartArray for debugging
+    // Log the Item
         console.log('Item added:', itemBeingAdded, 'Cart Array:', cartArray);
 
-        // Append the added item to the cartRows (assuming that cartRows is a container for cart items)
-        cartRows.appendChild(itemBeingAdded);
+        // Append the item to cartRows
+        if (cartRows.children.length > 0) {
+            cartRows.insertBefore(itemBeingAdded, cartRows.children[0]);
+        } else {
+            cartRows.appendChild(itemBeingAdded);
+        }
+        
+       
+       // Logs
+       
+        console.log('Cart Item appended')
+        console.log('Add to cart complete')
+        console.log('cartArray:',cartArray)
+      
     });
 });
+
+
+
+
+// Delete a item 
+
+
+let itemBeingDeleted;
+
+// Get the cloned item and add event listners to the del button?? 
+
+
+// Delete buttons: 
+
+const delBtns = cartRows.querySelectorAll('.cartRemove')
+cartRows.addEventListener('click', (e) => {
+    let deleteSuccesful = false
+    if (e.target.classList.contains('cartRemove')) {
+        // The click event was on a delete button
+        console.log('Delete Item called');
+        itemBeingDeleted = e.target.parentElement;
+        deleteSuccesful = true;
+        console.log('Item being deleted is:', itemBeingDeleted);
+        
+        
+        // Remove the item from the DOM: 
+        e.target.parentElement.remove()
+
+        // Remove the element from the array: 
+       const itemIndex = cartArray.indexOf(e.target.parentElement) // Or itemBeingDeleted just thought this is more future proof
+       if (itemIndex !== -1) {
+        cartArray.splice(itemIndex, 1);
+        
+
+
+        // Logs
+        
+        // Log wether the delete was succesful or not:
+        
+        if (deleteSuccesful) { 
+            console.log('deleteSuccesful:',deleteSuccesful);
+        }
+        else { 
+            console.log('delete Not succesful',deleteSuccesful)
+        }
+
+        // Log The array after the element has been removed: 
+        console.log('cartArray:',cartArray)
+        
+    }}})
+    
+
+
 
