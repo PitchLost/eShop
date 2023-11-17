3b60561ec9fb7e7b13e8ed43a6e5977220a880cb
PitchLost <thundergames221@gmail.com>
2023-11-17 16:23:34 +1300

    Main stock Items | Flex needs fixing on them

--- Search/search.js
+++ Search/search.js
@@ -2,6 +2,8 @@ let searchBar = document.getElementById('searchBar')
 const searchButton = document.getElementById('searchButton')
 const searchForm = document.getElementById('searchForm')
 
+let failedAttempts = 0
+
 searchForm.addEventListener('submit',e => { 
     e.preventDefault()
     let targetId = searchBar.value;
@@ -14,6 +16,13 @@ searchForm.addEventListener('submit',e => {
         // Scroll to the target element smoothly
         targetElement.scrollIntoView({ behavior: 'smooth' });
     } else {
-        alert("Element not found.");
+        alert("This Item dosent seem to exist. Check for any spelling errors and try again.");
+        failedAttempts++;
+        console.log('The amount of failed attemps are',failedAttempts)
+        if (failedAttempts > 1) { 
+            alert('If You are still having issues please report it using a bug report ')
+        } else if (failedAttempts <1) { 
+            return
+        }
     }
 });
